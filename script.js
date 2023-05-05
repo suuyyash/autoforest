bbfc24b8f563c9733af63e1391b3bcf88b4b457e
suuyyash <98825613+suuyyash@users.noreply.github.com>
2023-05-05 13:59:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const searchForm = document.querySelector('.search-form');
+const searchBtn = document.querySelector('#search-btn');
+const cartItem = document.querySelector('.cart-items-container')
+const cartBtn = document.querySelector('#cart-btn')
+const navbar = document.querySelector('.navbar')
+const menuBtn = document.querySelector('#menu-btn')
+var preloader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    preloader.style.display="none";
+})
+
+
+searchBtn.addEventListener('click', ()=>{
+    searchForm.classList.toggle('active');
+    document.addEventListener('click', (e)=>{
+        if(!e.composedPath().includes(searchBtn) && !e.composedPath().includes(searchForm) ){
+            searchForm.classList.remove('active');
+        }
+    })
+})
+
+cartBtn.addEventListener('click', ()=>{
+    cartItem.classList.toggle('active');
+    document.addEventListener('click', (e)=>{
+        if(!e.composedPath().includes(cartBtn) && !e.composedPath().includes(cartItem) ){
+            cartItem.classList.remove('active');
+        }
+    })
+})
+
+menuBtn.addEventListener('click', ()=>{
+    navbar.classList.toggle('active');
+    document.addEventListener('click', (e)=>{
+        if(!e.composedPath().includes(navbar) && !e.composedPath().includes(menuBtn) ){
+            navbar.classList.remove('active');
+        }
+    })
+})
+
